ff9c5fa71a31569a90c914a231215b4c67959c62
g-spn <107374574+g-spn@users.noreply.github.com>
2024-05-18 15:25:17 +0900

    Update The Game of Nimm.py

--- week4/The Game of Nimm.py
+++ week4/The Game of Nimm.py
@@ -6,20 +6,21 @@ Lose: picks up the last stone
 """
 
 def main():
-    #Welcome to the ancient game of Nimm
+    #initializing variables to act as counters
     stones = 20
     turn_counter = 0
+    
     while stones > 0:
         print("There are "+str(stones)+" stones left.")  # prints how many stones left
         
-        #displays current players turn
+        #Turn counter
+        turn_counter += 1
+        
+        #Used to determine current players' turn
         if turn_counter % 2 == 0:
             player = 1
         else:
             player = 2
-        
-        #turn counter
-        turn_counter += 1
         
         #asks the user how many stones to remove
         remove_stones = int(input("Player " + str(player) +" would you like to remove 1 or 2 stones? "))

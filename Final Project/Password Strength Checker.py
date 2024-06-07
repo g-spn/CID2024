55cc191315847295eb4fd987bfd1a7671ea9d4d6
g-spn <jpgspoon@gmail.com>
2024-06-07 20:12:36 +0900

    Provided goals and steps of Project

---
+++ Final Project/Password Strength Checker.py
@@ -0,0 +1,26 @@
+# File: Password Strength Checker
+# -----------------------------
+"""
+Step 1:
+    Define Password strenght Criterias
+Step 2:
+    Set up a way for user to input password
+Step 3:
+    Evaluate the password
+    Create multiply function for each seperate criteria
+Step 4: 
+    Combine resule from evaluating difference criterias
+Step 5:
+    Display feedback
+Step 6(optional):
+    Calculate the entropy of the password which provides the lenght of time it would take to brute froce
+    check users password to see if it's on the top used passwords
+
+
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
